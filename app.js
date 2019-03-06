0ed31a7a3e8170b26544edceefcc13034f08d66b
baja007 <baja007@wp.pl>
2019-03-06 22:46:33 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+
+
+document.querySelector('#register').addEventListener('click', function() {
+  let firstName = document.querySelector('#firstName').value;
+  let lastName = document.querySelector('#lastName').value;
+  let email = document.querySelector('#email').value;
+  
+  // firstName - 2-25 znaków
+  if (firstName.length >=2 && firstName.length <=25) {
+    alert('Name accepted');
+  } else {
+    alert('Name too long');
+  }
+  
+  if (typeof lastName == 'string') {
+//     alert('Podana wartosc jest stringiem');
+  }
+  
+  if (typeof email == 'string') {
+//     alert('Podana wartosc jest stringiem');
+  }
+  
+});
